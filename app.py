7d917b8437a301655e21c49d80939b41cfbcaf6f
berry-cc <berry.cc@gmail.com>
2026-02-25 16:50:49 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Health Line App is running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Received:", data)
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
